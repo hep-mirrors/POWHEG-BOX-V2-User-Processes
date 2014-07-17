b4da3f169c211d45b464fdcff76f6100fd2fd9f7
luisonig <luisonig@9e129b38-c2c0-4eec-9301-dc54fda404de>
2014-07-17 12:50:33 +0000

    Bug fix in maxmin script
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/User-Processes-V2@2782 9e129b38-c2c0-4eec-9301-dc54fda404de

--- ggHZ/maxminLHEF.sh
+++ ggHZ/maxminLHEF.sh
@@ -7,7 +7,7 @@ min) ind=5 ;;
 esac
 
 
-(echo $ind ; ls -c1 pwgLHEF_analysis-W[1245689].top ; echo "") | mergedata
+(echo $ind ; ls -c1 pwgLHEF_analysis-W[123456789].top ; echo "") | mergedata
 
 mv fort.12 pwgLHEF_analysis-$which.top
 
