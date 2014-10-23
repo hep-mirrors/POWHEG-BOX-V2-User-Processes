eeac697df41ebec534aec08f1d9b939823c7e0fb
oleari <oleari@9e129b38-c2c0-4eec-9301-dc54fda404de>
2014-10-23 14:46:36 +0000

    deleted old files
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/User-Processes-V2@2854 9e129b38-c2c0-4eec-9301-dc54fda404de

--- Wbbj/GoSamlib/compile_gosamlib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd $1/../GoSamlib/
-
-FILES=$(echo *.f *.f90 ' ' | sed 's/qlonshellcutoff.f// ; s/qlconstants.f// ; s/.f /.o /g ; s/.f90 /.o /g ; ')
-
-cd $1
-
-ar cru gosamlib.a $FILES
