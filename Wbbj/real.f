e50a7a985fa3d2a610724edd397967f6f18bbe6d
oleari <oleari@9e129b38-c2c0-4eec-9301-dc54fda404de>
2014-10-27 14:42:07 +0000

    fixed real.f
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/User-Processes-V2@2859 9e129b38-c2c0-4eec-9301-dc54fda404de

--- Wbbj/real.f
+++ Wbbj/real.f
@@ -37,12 +37,30 @@ c a (leading) color structure to an event.
       include 'nlegborn.h'
       include 'LesHouches.h'
       integer rflav(nlegreal),color(2,nlegreal)
-      integer i,j
+      integer i,j,itmp
+      integer idvecbos,vdecaymode
+      common/cvecbos/idvecbos,vdecaymode
       do i=1,nlegreal
          rflav(i)=idup(i)
          if (rflav(i).eq.21) rflav(i)=0
       enddo
+
+      if(idvecbos.eq.-24) then
+         call cconj(rflav,nlegreal)
+      endif
+
+
       call real_color(rflav,color)
+
+      if(idvecbos.eq.-24) then
+         call cconj(rflav,nlegreal)
+         do j=1,nlegreal
+            itmp = color(1,j)
+            color(1,j) = color(2,j)
+            color(2,j) = itmp
+         enddo
+      endif
+
       do i=1,2
          do j=1,nlegreal
             icolup(i,j)=color(i,j)
