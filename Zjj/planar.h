12ea252452b500171b8e95b8d8660f023ef7120a
nason <nason@9e129b38-c2c0-4eec-9301-dc54fda404de>
2014-11-06 07:07:46 +0000

    take it off!
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/User-Processes-V2@2874 9e129b38-c2c0-4eec-9301-dc54fda404de

--- Zjj/planar.h
+++
@@ -1,8 +0,0 @@
-c -*- Fortran -*-
-      integer ncstructmax
-      parameter (ncstructmax=2)
-      integer ncstruct,nclines,clineofpart(ncstructmax,nlegborn)
-      double precision sigmaofncstruct(ncstructmax)
-      common/planar/ncstruct,nclines,clineofpart,sigmaofncstruct
-      
-      
