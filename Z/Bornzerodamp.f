48e79414742d03078e01cf7431b2e2aae03dbd41
nason <nason@9e129b38-c2c0-4eec-9301-dc54fda404de>
2014-12-04 06:43:49 +0000

    add hnew_damp option
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/User-Processes-V2@2912 9e129b38-c2c0-4eec-9301-dc54fda404de

--- Z/Bornzerodamp.f
+++ Z/Bornzerodamp.f
@@ -10,22 +10,20 @@ c the real contribution to implement Born zero suppression
       include 'pwhg_flg.h'
       include 'pwhg_math.h'
       integer alr
-      real * 8 r0,rc,rs,rcs,rapp,dampfac,h,oh,mz2,pt2,
-     1     powheginput,dotp,amp,ratio
+      real * 8 r0,rc,rs,rcs,rapp,dampfac,h,oh,m2,pt2,
+     1     powheginput,dotp,amp,ratio,hnew_damp
       logical ini
       data ini/.true./
-      logical angcorr_damp,new_damp,new_damp_v
+      logical angcorr_damp,new_damp
       integer numopt
-      save ini,h,angcorr_damp,new_damp,new_damp_v
+      save ini,h,angcorr_damp,new_damp,hnew_damp
       external powheginput,dotp
       if(ini) then
          angcorr_damp = powheginput("#angcorr_damp") .eq. 1
          new_damp = powheginput("#new_damp") .eq. 1
-         new_damp_v = powheginput("#new_damp_v") .eq. 1
          numopt = 0
          if(angcorr_damp) numopt = numopt + 1
          if(new_damp) numopt = numopt + 1
-         if(new_damp_v) numopt = numopt + 1
          if(numopt .gt. 1) then
             write(*,*) ' bornzerodamp:'
             write(*,*) ' you should specify only one of'//
@@ -37,8 +35,14 @@ c the real contribution to implement Born zero suppression
             write(*,*) ' using angular correlations aware damp function'
          elseif(new_damp) then
             write(*,*) ' using new, better default damp function'            
-         elseif(new_damp_v) then
-            write(*,*)'using modified new, better default damp function'
+c if less than 0 will not be used
+            hnew_damp =  powheginput("#hnew_damp")
+            if(hnew_damp.gt.0) then
+               write(*,*) ' Using hnew_damp=',hnew_damp
+               if(hnew_damp.gt.1) then
+                  write(*,*) ' Warning: hnew_damp>1 not recommended'
+               endif
+            endif
          endif
          h=powheginput("#hdamp")
          if(h.lt.0) then
@@ -70,20 +74,16 @@ c the real contribution to implement Born zero suppression
             endif
          elseif(new_damp) then
             rapp = rc+rs-rcs
-            dampfac= min(1d0,rapp/r0)
-            dampfac = max(dampfac,0d0)
-         elseif(new_damp_v) then
-            rapp = rc+rs-rcs
-            if(h.gt.0) then
+            if(hnew_damp.gt.0) then
                pt2 = kn_cmpreal(1,5)**2+kn_cmpreal(2,5)**2
-               mz2 = 2*dotp(kn_cmpreal(:,3),kn_cmpreal(:,4))
-               rapp = rapp*h**2*mz2/(pt2+mz2*h**2)
+               m2 = 2*dotp(kn_cmpreal(:,3),kn_cmpreal(:,4))
+               rapp = rapp*hnew_damp**2*m2/(pt2+m2*hnew_damp**2)
             endif
             dampfac= min(1d0,rapp/r0)
             dampfac = max(dampfac,0d0)
-            return
          elseif(r0.gt.5*rc.and.r0.gt.5*rs) then
             dampfac=0
+c we might as well return here, dampfac already zero ...
             return
          else
             dampfac = 1
@@ -95,8 +95,8 @@ c the real contribution to implement Born zero suppression
 c local variables
       if(h.gt.0) then
          pt2 = kn_cmpreal(1,5)**2+kn_cmpreal(2,5)**2
-         mz2 = 2*dotp(kn_cmpreal(:,3),kn_cmpreal(:,4))
-         dampfac = dampfac*h**2*mz2/(pt2+mz2*h**2)
+         m2 = 2*dotp(kn_cmpreal(:,3),kn_cmpreal(:,4))
+         dampfac = dampfac*h**2*m2/(pt2+m2*h**2)
       endif
 
       if(dampfac.gt.1) dampfac = 1
