a914297c181b435d57f01b52ef5c02224709b70c
jaeger <jaeger@9e129b38-c2c0-4eec-9301-dc54fda404de>
2015-09-18 12:11:26 +0000

    add Makefile for bbH
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/User-Processes-V2@3112 9e129b38-c2c0-4eec-9301-dc54fda404de

--- bbH/virtual.f
+++ bbH/virtual.f
@@ -18,7 +18,6 @@ c Virtual needs to be provided by the user and put here
       save fakevirt 
 
       virtual = 0d0
-      call set_ebe_couplings
 
       if (firsttime) then
          fakevirt=powheginput("#fakevirt")
@@ -29,18 +28,18 @@ c Virtual needs to be provided by the user and put here
       if(fakevirt.eq.1) then  
 
 c calculate the RW matrix elements squared at the parton level:	
-         call bbh_interface(0,p,vflav,born,dum)
+         call bbh_virtual_interface(0,p,vflav,born,dum)
          virtual = 0.2d0*born
  
       else !true virtuals   
 
 c calculate the RW matrix elements squared at the parton level:	
-         call bbh_interface(1,p,vflav,born,virt_amp2)
+         call bbh_virtual_interface(1,p,vflav,born,virt_amp2)
          virtual = virt_amp2
 
 c convert to POWHEG normalization:
          virtual = virtual*(2d0*pi/st_alpha)
-         
+
       endif   
 
       end
