26757fe4de3d9bb9c144a98c730d16d4f1e4fc48
re <re@9e129b38-c2c0-4eec-9301-dc54fda404de>
2017-07-25 16:03:05 +0000

    Updated local copy of lhapdfif.f and added a local version of lhapdf6if.f. These are needed to make sure that alphaspdf can be used without problems also when using a 4f PDF (obviously this is needed here).
    Makefile: trivial changes
    
    
    
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/User-Processes-V2@3428 9e129b38-c2c0-4eec-9301-dc54fda404de

--- ST_tch_4f/lhapdf6if.f
+++ ST_tch_4f/lhapdf6if.f
@@ -1,43 +1,56 @@
       subroutine genericpdfset(ndns)
       implicit none
-c wrap for pdfset; avoids subsequent
-c calls to pdfset (you never know)
       include 'pwhg_pdf.h'
       integer ndns
-      character * 20 parm(20)
-      real * 8 val(20)
-      integer ndns0
+c wrap for pdfset; avoids subsequent
+c calls to pdfset (you never know)
+      integer,allocatable :: seq(:),idset(:),nsets
+      real * 8, allocatable :: lam5set(:),q2minset(:)
+      save lam5set,q2minset,seq,idset,nsets
       real * 8 genericxlambdL,genericxlambdNL,genericxlambdNNL,
-     ,         alphasPDF,asmz,mz,lam5
+     ,         alphasPDF,asmz,mz
       parameter (mz=91.1876d0)
-      integer iord
-      common/cgenericpdf/lam5,iord
-      real * 8 qcdl4,qcdl5
-      common/w50512/qcdl4,qcdl5
-      data ndns0/-1/
-      save ndns0
-      if(ndns.ne.ndns0) then
-         ndns0=ndns
-         parm(1)='DEFAULT'
-         val(1)=ndns
-         call pdfset(parm,val)
-
-
-c     find q2min and store it in common block 
-         call GetQ2min(0,pdf_q2min)
-
-c     Workaround for lhapdf-6 bug:
-c     re-initialise the pdf set, since getq2min in lhapdf v6
-c     can change what was chosen with the first call to pdfset
-         ndns0=ndns
-         parm(1)='DEFAULT'
-         val(1)=ndns
-         call pdfset(parm,val)
-     
-
-         asmz=alphasPDF(mz)
+      real * 8 lam5
+      integer iord,iset,maxsets
+      common/cgenericpdf/lam5,iord,iset,maxsets
+      logical,save:: ini=.true.
+      integer j
+      real * 8 powheginput
+      if(ini) then
+         nsets = 0
+         iset = 0
+         maxsets=nint(powheginput("#lhapdf6maxsets"))
+         if(maxsets < 0) maxsets = 10
+         allocate(seq(maxsets),idset(maxsets),
+     1        lam5set(maxsets),q2minset(maxsets))
+         call setlha6init(maxsets)
+         ini = .false.
+      endif
+      if(iset /= 0) then
+         if(idset(iset) == ndns) return
+      endif
+      do j=1,nsets
+         if(ndns == idset(j)) then
+            iset = j
+            call ontop(iset)
+            exit
+         endif
+      enddo
+      if(j == nsets+1) then
+c...  initalise set
+         if(nsets == maxsets) then
+c     delete the oldest
+            iset = seq(1)
+            call setlha6del(iset)
+            call ontop(iset)
+         else
+            nsets = nsets+1
+            iset = nsets
+            seq(iset)=iset
+         endif
+         idset(iset) = ndns
+         call setlha6set(iset,ndns,iord,mz,asmz,pdf_q2min)
          write(*,*) ' check: alpha_s(Mz)=',asmz
-         call GetOrderAs(iord)
          if(iord.eq.0) then
 c better to use the NLO formula anyhow;
 c we don't have LO alpha around
@@ -49,7 +62,6 @@ c we don't have LO alpha around
          endif
          write(*,*) ' alpha_s order (0,1,2): ',iord
          write(*,*) ' Lambda 5 is ',lam5
-         write(*,*) ' LHA value is ',qcdl5
 
 ccccccccccccccccccccccccccccccccccccccccccccccccccc
 c     !ER:
@@ -57,26 +69,28 @@ c     needed to have pwhg_alphas=alphasPDF(q)
 c     (i.e. POW alpha to match pdf's alpha)
          lam5=genericxlambdNL(asmz,mz,4)
          write(*,*) ' Lambda 4 is ',lam5
-         write(*,*) ' LHA value is ',qcdl4
-c$$$         call GetNf(nfmax)
-c$$$         print*, nfmax
-c$$$         call GetThreshold(3,Q)
-c$$$         print*, Q
-c$$$         call GetThreshold(4,Q)
-c$$$         print*, Q
-c$$$         call GetThreshold(5,Q)
-c$$$         print*, Q
-c$$$         call GetThreshold(6,Q)
-c$$$         print*, Q
-c$$$         stop
 ccccccccccccccccccccccccccccccccccccccccccccccccccc
          
-c         if(iord.lt.2) then
-c            write(*,*) ' alfa(mz):',alfas(mz**2,lam5,5)
-c         else
-c            write(*,*) ' alfa(mz):',alfa3(mz**2,lam5,5)
-c         endif
+         lam5set(iset) = lam5
+         q2minset(iset) = pdf_q2min
+      else
+         lam5 = lam5set(iset)
+         pdf_q2min = q2minset(iset)
       endif
+      contains
+      subroutine ontop(j)
+c     look for j in seq
+      integer j,k,l
+      do k=1,nsets
+         if(seq(k) == j) then
+            do l=k,nsets-1
+               seq(l)=seq(l+1)
+            enddo
+            seq(nsets)=j
+            return
+         endif
+      enddo
+      end subroutine
       end
 
       function genericxlambdL(as,q,nf)
@@ -162,6 +176,12 @@ c Interface to lhapdf package.
       implicit none
       include 'nlegborn.h'
       include 'pwhg_pdf.h'
+
+      real * 8 lam5
+      integer iord,iset,maxsets
+      common/cgenericpdf/lam5,iord,iset,maxsets
+
+
       integer ndns,ih
       real * 8 xmu2,x,fx(-pdf_nparton:pdf_nparton)
       real * 8 fxlha(-6:6)
@@ -170,14 +190,14 @@ c Interface to lhapdf package.
       real*8 photon
       call genericpdfset(ndns)
 
+      call xfxq2(iset,x,xmu2,fxlha)
 c photon induced work only with MRST2004QED (ndns = 20460)
       if (ndns.eq.20460) then
-          call evolvePDFphoton(x,sqrt(xmu2),fxlha,photon)
+          call xfphoton(x,sqrt(xmu2),photon)
       else
-          call evolvePDF(x,sqrt(xmu2),fxlha)
           photon=0d0
       endif
-c pftopdg returns density times x
+
       fx=0
       fx(-6:6)=fxlha/x
       if(pdf_nparton.ge.22) then
@@ -225,13 +245,16 @@ c      endif
 
       subroutine genericpdfpar(ndns,ih,xlam,scheme,iorder,iret)
       implicit none
+
+      integer iord,iset,maxsets
+      real * 8 lam5
+      common/cgenericpdf/lam5,iord,iset,maxsets
+
       integer ndns,ih,iorder
       real * 8 xlam
       character * 2 scheme
       integer iret
-      real * 8 lam5
-      integer iord
-      common/cgenericpdf/lam5,iord
+
       call genericpdfset(ndns)
       scheme='MS'
       iret=0
@@ -242,4 +265,15 @@ c      endif
       function whichpdfpk()
       character * 3 whichpdfpk
       whichpdfpk='lha'
+      end
+
+      function alphaspdf(mu)
+      implicit none
+      real * 8 lam5
+      integer iord,iset,maxsets
+      common/cgenericpdf/lam5,iord,iset,maxsets
+      real *8 mu,alphaspdf,tmp
+      call alphasfrompdf(iset,mu,tmp)
+      alphaspdf=tmp
+      return
       end
