9dc0ba75bcf07c49a050a72c4f8bcbf9edcae90f
heinrich <heinrich@9e129b38-c2c0-4eec-9301-dc54fda404de>
2020-06-30 13:52:34 +0000

    new version which allows to vary 5 couplings within a non-linear EFT (EWChL)
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/User-Processes-V2@3757 9e129b38-c2c0-4eec-9301-dc54fda404de

--- ggHH/Herwig7.1/herwigsettings.h
+++
@@ -1,4 +0,0 @@
-      integer eventCounter
-      real * 8 scalupfac,ub_btilde_corr,ub_remn_corr, ub_corr
-      common/herwigSettings/ scalupfac,ub_btilde_corr,
-     2     ub_remn_corr,eventCounter
--- ggHH/shell/runNLO.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-(echo 1 ; ls -c1 pwg-*-NLO.top ; echo "") | $(dirname $0)/mergedata
-
-mv fort.12 pwgNLO.top
-
-
